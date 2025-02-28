b335bc1a43160382ccba79928305fdc3b87938c8
fjribptra <bagjaputrafajri@gmail.com>
2025-03-01 05:54:55 +0700

    seo: add metadata keyword, robots, and content type

--- src/components/HTML.tsx
+++ src/components/HTML.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 
-const HTML = ({children}: {children: React.ReactNode}) => {
+const HTML = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <meta name="google-site-verification" content="BaZI1dEoU-chgF1oO_CktflBxhmfCnUEsop82e3DkL8" />
+      <meta name="keywords" content="ringkasin, ai, article summarizer" />
+      <meta name="robots" content="index, follow" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       {children}
     </html>
-  )
-}
+  );
+};
 
-export default HTML
+export default HTML;
